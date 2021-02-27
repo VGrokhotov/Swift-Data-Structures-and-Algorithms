81dd0f2f1f62354a40ce8c45b83fa844c26e7d0f
Vladislav <50099240+VGrokhotov@users.noreply.github.com>
2021-02-27 20:26:04 +0300

    update to class

--- Stack.swift
+++ Stack.swift
@@ -1,24 +1,25 @@
-public struct Stack<T> {
 
-  private var array = [T]()
-  
-  public var isEmpty: Bool {
-    return array.isEmpty
-  }
-  
-  public var count: Int {
-    return array.count
-  }
-  
-  public mutating func push(_ element: T) {
-    array.append(element)
-  }
-  
-  public mutating func pop() -> T? {
-    return array.popLast()
-  }
-  
-  public var top: T? {
-    return array.last
-  }
+public final class Stack<T> {
+    
+    private var array = [T]()
+    
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    public var count: Int {
+        return array.count
+    }
+    
+    public func push(_ element: T) {
+        array.append(element)
+    }
+    
+    public func pop() -> T? {
+        return array.popLast()
+    }
+    
+    public var top: T? {
+        return array.last
+    }
 }
